84c91f228559999c657f4132d5617990066905de
s1n7ax <srineshnisala@gmail.com>
2022-07-20 18:22:54 +0530

    Revert "feat: add lua snips"
    This reverts commit 07c9fb71fcda69a78db202ccfbadc80ef6aeddce.

---
+++ lua/greetings/awesome-module.lua
@@ -0,0 +1,5 @@
+local function greet()
+    print('hello world')
+end
+
+return greet
--- lua/snips/lua/stdout.lua
+++
@@ -1,7 +0,0 @@
-local ls = require("luasnip")
-local fmt = require("luasnip.extras.fmt").fmt
-local i = ls.insert_node
-
-return fmt("print({})", {
-	i(1, "value"),
-})
